f527ea08c6d76939d7a24a2c2131e5637beaadae
Lovtzoff <lovtzoff.aleksei@gmail.com>
2023-03-29 16:28:30 +0300

    Fix ApplicationConfiguration
    Add properties for the JdbcTemplate to work.

--- src/main/java/ru/clevertec/dao/rowmapper/ProductRowMapper.java
+++ src/main/java/ru/clevertec/dao/rowmapper/ProductRowMapper.java
@@ -1,11 +1,13 @@
 package ru.clevertec.dao.rowmapper;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
 import ru.clevertec.model.Product;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Service
 public class ProductRowMapper implements RowMapper<Product> {
 
     @Override
